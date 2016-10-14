6270b398b697a0e0f07f337c8681b74a6e03daa5
mjdude <mo@syncspot.net>
2016-10-13 22:47:11 -0400

    example difference with states

--- app/components/ComponentOne.jsx
+++ app/components/ComponentOne.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 var ComponentOne = React.createClass({
+  getInitialState: function() {
+    return {
+      count: 0
+    }
+  },
   render: function() {
     return (
       <div>
         <h3>Component One Using React.createClass</h3>
+        <p>Current Count: {this.state.count}</p>
       </div>
     );
   }
