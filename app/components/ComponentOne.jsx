a7b426de8d87da01d3ac6458e7b7c25fa32eceda
mjdude <mo@syncspot.net>
2016-10-13 22:41:13 -0400

    created react component with create class and with extension

---
+++ app/components/ComponentOne.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var ComponentOne = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h3>Component One Using React.createClass</h3>
+      </div>
+    );
+  }
+});
+
+export default ComponentOne;
---
+++ app/components/ComponentTwo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ComponentTwo extends React.Component {
+  render () {
+    return (
+      <div>
+        <h3>Component Two Using React.Component</h3>
+      </div>
+    )
+  }
+}
+
+
+export default ComponentTwo;
